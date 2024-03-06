0e9cf515f7a7e89d595021fc020b53681a4b42e8
termaulmaul <maulanarafinurdiansyah22@gmail.com>
2024-03-07 04:08:50 +0700

    done for praktikum02

--- praktikum_02/main.dart
+++ praktikum_02/main.dart
@@ -1,82 +1,22 @@
-void main() {
-//   var angka = 5;
-//   var grade = "B";
-
-//   print("Hello, World!"); //
-
-//   var name = 'Puspita';
-//   var year = 2023;
-//   var bulan = ['Januari', 'Februari', 'Maret', 'April'];
-//   var image = {
-//     'buah': ['mangga'],
-//     'url': '//path/to/mangga.jpg'
-//   };
-
-//   if (angka > 0) {
-//     print('bilangan positif');
-//   } else if (angka < 0) {
-//     print('bilangan negatif');
-//   }
-
-//   switch (grade) {
-//     case "A":
-//       print("GGWP");
-//       break;
-//     case "B":
-//       print("GG");
-//       break;
-//   }
-
-//   for (int i = 1; i <= 100; i++) {
-//     print(i);
-//   }
-
-//   List<String> month = ['Januari', 'Februari', 'Maret', 'April'];
-
-//   var angkaSet = {1, 4, 6, 4, 1};
-//   Set<int> bilanganSet = new Set.from(angkaSet);
-//   print(bilanganSet);
-
-  var kucing = Hewan("Maman", 2, 5.3);
-  kucing.makan();
-  kucing.tidur();
-
-  var kucing2 = Meong("Cimol", 2, 5.0, "Oren");
-  kucing2.jalan();
-}
-
-class Hewan {
-  String nama;
-  int umur;
-  double berat;
-
-  Hewan(this.nama, this.umur, this.berat);
-
-  void makan() {
-    print('$nama makan.');
-    berat = berat + 0.2;
-  }
-
-  void tidur() {
-    print('$nama sedang tidur');
+void main() async {
+  print('Getting your product...');
+  try {
+    var order = await getProduct();
+    print('You order: $order');
+  } catch (error) {
+    print('Sorry. $error');
+  } finally {
+    print('Thank you');
   }
 }
 
-class Meong extends Hewan {
-  String warnaBulu;
-
-  Meong(
-    String nama,
-    int umur,
-    double berat,
-    this.warnaBulu,
-  ) : super(nama, umur, berat);
-
-  void jalan() {
-    print('$nama berjalan');
-  }
-
-  void warna() {
-    print('$nama berwarna $warnaBulu');
-  }
+Future<String> getProduct() {
+  return Future.delayed(Duration(seconds: 3), () {
+    var isProductAvailable = false;
+    if (isProductAvailable) {
+      return 'Matcha Latte';
+    } else {
+      throw 'Our stock is not enough.';
+    }
+  });
 }
