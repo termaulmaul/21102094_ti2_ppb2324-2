5fd14bdf48a3c4ccb098a9a334f16cebf06a18bd
termaulmaul <maulanarafinurdiansyah22@gmail.com>
2024-03-07 11:16:50 +0700

    add MVVIM(Pertemuan3)

---
+++ MVVIM(Pertemuan3)/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
