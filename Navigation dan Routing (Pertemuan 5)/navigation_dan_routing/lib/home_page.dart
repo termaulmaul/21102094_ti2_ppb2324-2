eb400a106226930f6e84fc2f6a80745e615b0065
Maulana Rafi <maulanarafinurdiansyah22@gmail.com>
2024-03-21 11:01:39 +0700

    add all file

---
+++ Navigation dan Routing (Pertemuan 5)/navigation_dan_routing/lib/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'second_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        backgroundColor: const Color.fromARGB(255, 54, 136, 244),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SecondPage(
+                      data: 'Data dari Home Page',
+                    ),
+                  ),
+                );
+              },
+              child: const Text('Menuju halaman kedua'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/second_page',
+                    arguments: 'Menggunakan route');
+              },
+              child: const Text('Menuju halaman kedua dengan route'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
