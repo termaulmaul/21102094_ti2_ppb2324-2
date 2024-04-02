2c2aa656d9f73cc58674ade3c925b522b9c11235
Maulana Rafi <maulanarafinurdiansyah22@gmail.com>
2024-04-02 09:37:29 +0700

    saving_app complete not all

--- saving_app (Pertemuan 6-7)/saving_app/test/widget_test.dart
+++ saving_app (Pertemuan 6-7)/saving_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:saving_app/main.dart';
+import 'package:managment/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
