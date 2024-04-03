943d74f97005f4e94cddbbf92a8fae8086344364
Maulana Rafi <maulanarafinurdiansyah22@gmail.com>
2024-04-03 09:17:11 +0700

    fixing conflic saving_app

---
+++ saving_app (Pertemuan 6-7)/saving_app/lib/app_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Expense Tracker",
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          background: Colors.grey.shade100,
+          onBackground: Colors.black,
+          primary: Color(0xFF00E2B7),
+          secondary: Color(0xFFE064F7),
+          tertiary: Color(0xFFFF8D6C),
+        )
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
