252347c83fe8c92c0a95dce3726ffebfedd76325
Maulana Rafi <maulanarafinurdiansyah22@gmail.com>
2024-04-03 09:10:13 +0700

    fixing saving_app

--- saving_app (Pertemuan 6-7)/saving_app/android/app/src/main/kotlin/com/example/managment/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.managment
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- saving_app (Pertemuan 6-7)/saving_app/lib/main.dart
+++ saving_app (Pertemuan 6-7)/saving_app/lib/main.dart
@@ -1,26 +1,6 @@
+import 'package:saving_app/app.dart';
 import 'package:flutter/material.dart';
-import 'package:managment/Screens/home.dart';
-import 'package:managment/Screens/statistics.dart';
-import 'package:managment/widgets/bottomnavigationbar.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-import 'data/model/add_date.dart';
-
-void main() async {
-  await Hive.initFlutter();
-  Hive.registerAdapter(AdddataAdapter());
-  await Hive.openBox<Add_data>('data');
+void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Bottom(),
-    );
-  }
 }
